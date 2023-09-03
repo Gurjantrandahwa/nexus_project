8f8e48b52d8c8154dc07b6222d8adacb7f319c26
Gurjant Singh <gurjantrandhawa74456@gmail.com>
2023-09-03 11:25:43 +0530

    sections implemented

--- src/Components/BodyCare/BodyCare.js
+++ src/Components/BodyCare/BodyCare.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./bodyCare.scss";
 import {BsArrowRightShort} from "react-icons/bs";
 import Carousel from "react-grid-carousel";
 import soap1 from "../../Assets/soap1.webp";
 import soap2 from "../../Assets/soap2.webp";
 import soap3 from "../../Assets/soap3.webp";
+import ProgressBar from "../ProgressBar/ProgressBar";
 
 const BodyCare = () => {
+    const [currentPage, setCurrentPage] = useState(0);
+
+    const handlePageChange = (newPage) => {
+        setCurrentPage(newPage);
+    };
+    const soapItems = [
+        {imgSrc: soap1, title: 'Nurture Bar Soap', description: 'Offers a mild yet effective cleanse'},
+        {imgSrc: soap2, title: 'Polish Bar Soap', description: 'Offers a thorough and enlivening cleanse'},
+        {imgSrc: soap3, title: 'Refresh Bar Soap', description: 'Offers a mild yet effective cleanse'},
+    ];
     return <section className="carousel-container">
-        <Carousel  cols={2} rows={1}>
+        <Carousel cols={2} rows={1} onPageChange={handlePageChange}>
             <Carousel.Item>
                 <h3>For the body</h3>
-                <h2 className={"sub-title"}>An expression of care</h2>
+                <h2 className="sub-title">An expression of care</h2>
                 <p>
                     Aesop formulations offer the body deserving care, to cleanse, replenish, and nourish skin. Each
                     product is a sensory pleasure to use with its own delightful aroma.
@@ -19,39 +30,20 @@ const BodyCare = () => {
                 <button>
                     See all Body Care <BsArrowRightShort/>
                 </button>
-
             </Carousel.Item>
 
-            <Carousel.Item>
-
-                <img src={soap1} alt="Nurture Bar Soap" className={"body-care-img"}/>
-                <div className={"soap-text-wrapper"}>
-                    <h3 className={"soap-title"}>Nurture Bar Soap</h3>
-                    <p>Offers a mild yet effective cleanse</p>
-
-                </div>
-
-            </Carousel.Item>
-
-            <Carousel.Item>
-
-                <img src={soap2} alt="Polish Bar Soap" className={"body-care-img"}/>
-                <div className={"soap-text-wrapper"}>
-                    <h3 className={"soap-title"}>Polish Bar Soap</h3>
-                    <p>Offers a thorough and enlivening cleanse</p>
-                </div>
-
-            </Carousel.Item>
-
-            <Carousel.Item>
-                <img src={soap3} alt="Refresh Bar Soap" className={"body-care-img"}/>
-                <div className={"soap-text-wrapper"}>
-                    <h3 className={"soap-title"}>Refresh Bar Soap</h3>
-                    <p>Offers a mild yet effective cleanse</p>
-                </div>
-
-            </Carousel.Item>
+            {soapItems.map((soap, index) => (
+                <Carousel.Item key={index}>
+                    <img src={soap.imgSrc} alt={soap.title} className="body-care-img"/>
+                    <div className="soap-text-wrapper">
+                        <h3 className="soap-title">{soap.title}</h3>
+                        <p>{soap.description}</p>
+                    </div>
+                </Carousel.Item>
+            ))}
         </Carousel>
+
+        <ProgressBar currentPage={currentPage} totalPages={soapItems.length}/>
     </section>
 };
 
--- src/Components/SkinCare/SkinCare.js
+++ src/Components/SkinCare/SkinCare.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Carousel from "react-grid-carousel";
 import {BsArrowRightShort} from "react-icons/bs";
 import skin1 from "../../Assets/skin1.png";
@@ -9,14 +9,66 @@ import skin5 from "../../Assets/skin5.webp";
 import skin6 from "../../Assets/skin6.webp";
 import skin7 from "../../Assets/skin7.webp";
 import skin8 from "../../Assets/skin8.webp";
+import ProgressBar from "../ProgressBar/ProgressBar";
 
+const skinData = [
+    {
+        image: skin1,
+        title: "Lucent Facial Concentrate",
+        description: "A Vitamin C-rich layering serum",
+    },
+    {
+        image: skin2,
+        title: "Purifying Facial Cream Cleanser",
+        description: "A daily cream cleanser for dry skin",
+    },
+    {
+        image: skin3,
+        title: "Camellia Nut Facial Hydrating Cream",
+        description: "For normal, dry or sensitive skin",
+    },
+    {
+        image: skin4,
+        title: "Parsley Seed Anti-Oxidant Eye Cream",
+        description: "Nourishes delicate skin around eyes",
+    },
+    {
+        image: skin5,
+        title: "B Triple C Facial Balancing Gel",
+        description: "Anti-oxidant gel infused with Vitamin B and C",
+    },
+    {
+        image: skin6,
+        title: "Mandarin Facial Hydrating Cream",
+        description: "Rapidly absorbed, lightly hydrating",
+    },
+    {
+        image: skin7,
+        title: "Parsley Seed Anti-Oxidant Intense Serum",
+        description: "A hydrating serum to bolster skin",
+    },
+    {
+        image: skin8,
+        title: "Parsley Seed Facial Cleanser",
+        description: "For those in polluted urban environments",
+    },
+];
 
 const SkinCare = () => {
+    const [currentPage, setCurrentPage] = useState(0);
+
+    const handlePageChange = (newPage) => {
+        console.log("Page changed to", newPage);
+        setCurrentPage(newPage);
+    };
+
     return <section className="carousel-container">
-        <Carousel className="b-care-wrapper" cols={3} rows={1}>
+        <Carousel className="b-care-wrapper" cols={3} rows={1}
+                  onPageChange={(page) => handlePageChange(page)}
+        >
             <Carousel.Item>
                 <h3>For the skin</h3>
-                <h2 className={"sub-title"}>Attention for all types</h2>
+                <h2 className="sub-title">Attention for all types</h2>
                 <p>
                     The well-being of your skin is the product of hydration, nourishment, and protection through
                     discerning rituals. Explore requisite skin care for all skin types.
@@ -24,79 +76,19 @@ const SkinCare = () => {
                 <button>
                     See all Skin Care <BsArrowRightShort/>
                 </button>
-
             </Carousel.Item>
 
-            <Carousel.Item>
-
-                <img src={skin1} alt="Nurture Bar Soap" className={"skin-care-img"}/>
-                <div className={"skin-text-wrapper"}>
-                    <h3 className={"skin-title"}>Lucent Facial Concentrate</h3>
-                    <p>A Vitamin C-rich layering serum</p>
-
-                </div>
-
-            </Carousel.Item>
-
-            <Carousel.Item>
-
-                <img src={skin2} alt="Polish Bar Soap" className={"skin-care-img"}/>
-                <div className={"skin-text-wrapper"}>
-                    <h3 className={"skin-title"}>Purifying Facial Cream Cleanser</h3>
-                    <p>A daily cream cleanser for dry skin</p>
-                </div>
-
-            </Carousel.Item>
-
-            <Carousel.Item>
-                <img src={skin3} alt="Refresh Bar Soap" className={"skin-care-img"}/>
-                <div className={"skin-text-wrapper"}>
-                    <h3 className={"skin-title"}>Camellia Nut Facial Hydrating Cream</h3>
-                    <p>For normal, dry or sensitive skin</p>
-                </div>
-
-            </Carousel.Item>
-            <Carousel.Item>
-                <img src={skin4} alt="Refresh Bar Soap" className={"skin-care-img"}/>
-                <div className={"skin-text-wrapper"}>
-                    <h3 className={"skin-title"}>Parsley Seed Anti-Oxidant Eye Cream</h3>
-                    <p>Nourishes delicate skin around eyes</p>
-                </div>
-
-            </Carousel.Item>
-            <Carousel.Item>
-                <img src={skin5} alt="Refresh Bar Soap" className={"skin-care-img"}/>
-                <div className={"skin-text-wrapper"}>
-                    <h3 className={"skin-title"}>B Triple C Facial Balancing Gel</h3>
-                    <p>Anti-oxidant gel infused with Vitamin B and C</p>
-                </div>
-
-            </Carousel.Item>
-            <Carousel.Item>
-                <img src={skin6} alt="Refresh Bar Soap" className={"skin-care-img"}/>
-                <div className={"skin-text-wrapper"}>
-                    <h3 className={"skin-title"}>Mandarin Facial Hydrating Cream</h3>
-                    <p>Rapidly absorbed, lightly hydrating</p>
-                </div>
-
-            </Carousel.Item>
-            <Carousel.Item>
-                <img src={skin7} alt="Refresh Bar Soap" className={"skin-care-img"}/>
-                <div className={"skin-text-wrapper"}>
-                    <h3 className={"skin-title"}>Parsley Seed Anti-Oxidant Intense Serum</h3>
-                    <p>A hydrating serum to bolster skin</p>
-                </div>
-
-            </Carousel.Item>
-            <Carousel.Item>
-                <img src={skin8} alt="Refresh Bar Soap" className={"skin-care-img"}/>
-                <div className={"skin-text-wrapper"}>
-                    <h3 className={"skin-title"}>Parsley Seed Facial Cleanser</h3>
-                    <p>For those in polluted urban environments</p>
-                </div>
-
-            </Carousel.Item>
+            {skinData.map((item, index) => (
+                <Carousel.Item key={index}>
+                    <img src={item.image} alt={item.title} className="skin-care-img"/>
+                    <div className="skin-text-wrapper">
+                        <h3 className="skin-title">{item.title}</h3>
+                        <p>{item.description}</p>
+                    </div>
+                </Carousel.Item>
+            ))}
         </Carousel>
+        <ProgressBar currentPage={currentPage} totalPages={skinData.length}/>
     </section>
 };
 
